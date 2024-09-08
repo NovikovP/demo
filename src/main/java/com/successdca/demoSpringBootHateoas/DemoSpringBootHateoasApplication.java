9baa31eccf4da9e5b01b7075cf6e53c984c69ec5
Novikov_Pavel <successdca@gmail.com>
2024-09-08 21:56:31 +0300

    init commit

---
+++ src/main/java/com/successdca/demoSpringBootHateoas/DemoSpringBootHateoasApplication.java
@@ -0,0 +1,13 @@
+package com.successdca.demoSpringBootHateoas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootHateoasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootHateoasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/successdca/demoSpringBootHateoas/DemoSpringBootHateoasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.successdca.demoSpringBootHateoas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringBootHateoasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
